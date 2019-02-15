5ac4fd54f711957be4472537e9f615b77cbb1ec4
Quentin Leon <qwolf1999@gmail.com>
2019-02-15 14:29:18 -0800

    edited farming behavior

--- new_bots/behavior.lua
+++ new_bots/behavior.lua
@@ -47,7 +47,7 @@ function Farm()
 
 
 	----Last-hit Creep----
-	if (eWeakestCreep ~= nil and eCreepHealth <= npcBot:GetAttackDamage() * 2.5) then
+	if (eWeakestCreep ~= nil and eCreepHealth <= npcBot:GetAttackDamage() * 3) then
 		if (eCreepHealth <= npcBot:GetAttackDamage() or #aCreeps == 0) then
 			if (GetUnitToUnitDistance(npcBot,WeakestCreep) <= attackRange) then
 				npcBot:Action_AttackUnit(eWeakestCreep, false)
