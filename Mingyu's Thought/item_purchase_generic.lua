fe8aad15e440eef18f1d86c594d45ce0b0f1bb3a
2ne1ugly <47616772+2ne1ugly@users.noreply.github.com>
2019-02-17 09:47:30 -0800

    Some ideas this morning

---
+++ Mingyu's Thought/item_purchase_generic.lua
@@ -0,0 +1,60 @@
+firstBuyItems = {
+	--["orb"] = "item_orb_of_venom",
+	--["branch0"] = "item_branches"
+	["quelling"] = "item_quelling_blade"
+}
+
+local tableItemsToBuy = { 
+				"item_tango",
+				"item_tango",
+				"item_clarity",
+				"item_clarity",
+				"item_branches",
+				"item_branches",
+				"item_magic_stick",
+				"item_circlet",
+				"item_boots",
+				"item_energy_booster",
+				"item_staff_of_wizardry",
+				"item_ring_of_regen",
+				"item_recipe_force_staff",
+				"item_point_booster",
+				"item_staff_of_wizardry",
+				"item_ogre_axe",
+				"item_blade_of_alacrity",
+				"item_mystic_staff",
+				"item_ultimate_orb",
+				"item_void_stone",
+				"item_staff_of_wizardry",
+				"item_wind_lace",
+				"item_void_stone",
+				"item_recipe_cyclone",
+				"item_cyclone",
+			};
+
+function ItemPurchaseThink()
+	local npcBot = GetBot()
+
+	--if (GetGameState() == 4) then
+		firstbuy()
+	--else
+		--earlyGameBuy()
+	--end
+end
+
+function earlyGameBuy()
+	local npcBot = GetBot()
+
+	if npcBot:GetGold() >= 600 then
+		npcBot:ActionImmediate_PurchaseItem("item_boots")
+	end
+end
+
+function firstbuy()
+	local npcBot = GetBot()
+	for _, item in pairs(firstBuyItems) do
+		npcBot:ActionImmediate_PurchaseItem(item)
+	end
+end
+
+return tableItemsToBuy
