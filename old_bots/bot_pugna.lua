9ad335237baf2afee0b7a081ec437d596e71f989
Quentin Leon <qwolf1999@gmail.com>
2019-02-18 13:27:55 -0800

    cleaned up the repo

--- old_bots/bot_pugna.lua
+++
@@ -1,8 +0,0 @@
-local module = require(GetScriptDirectory().."/functions")
-local bot_generic = require(GetScriptDirectory().."/bot_generic")
-
-local npcBot = GetBot()
-
-function Think()
-    bot_generic.Think()
-end
