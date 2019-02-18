e370a404f1b1bae0460e4e81156abb75471376b8
2ne1ugly <47616772+2ne1ugly@users.noreply.github.com>
2019-02-17 17:30:01 -0800

    items

---
+++ bots/item_purchase_phantom_assassin.lua
@@ -0,0 +1,47 @@
+local module = require(GetScriptDirectory().."/helpers")
+
+local Items = {
+	----battlefury----
+	"item_quelling_blade",
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_recipe_bfury",
+
+	----battlefury----
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_quelling_blade",
+	"item_recipe_bfury",
+
+	----battlefury----
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_quelling_blade",
+	"item_recipe_bfury",
+
+	----battlefury----
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_quelling_blade",
+	"item_recipe_bfury",
+
+	----battlefury----
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_quelling_blade",
+	"item_recipe_bfury",
+
+	----battlefury----
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_quelling_blade",
+	"item_recipe_bfury"
+}
+
+return Items
