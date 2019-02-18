58e0be3bf49af66d1efeac780c241b1de2089cbe
Jacob <43463199+j6i@users.noreply.github.com>
2019-02-17 20:44:50 -0800

    bane updated items

--- bots/item_purchase_bane.lua
+++ bots/item_purchase_bane.lua
@@ -2,28 +2,39 @@ local module = require(GetScriptDirectory().."/helpers")
 
 local Items =
 {
+	----archane boots----
 	"item_boots",
 	"item_energy_booster",
-	"item_void_stone",
-	"item_energy_booster",
-	"item_recipe_aether_lens",
+	"item_recipe_arcane_boots",
 
-	"item_cloak",
-	"item_shadow_amulet",
+	----null talisman----
+	"item_circlet",
+	"item_mantle",
+	"item_recipe_null_talisman",
 
-	"item_staff_of_wizardry",
-	"item_void_stone",
-	"item_recipe_cyclone",
-	"item_wind_lace",
+	----Necronomicon---
+	"item_sobi_mask",
+	"item_sobi_mask",
+	"item_belt_of_strength",
+	"item_recipe_necronomicon",
 
+	----black king bar----
+	"item_mithril_hammer",
+	"item_ogre_axe",
+	"item_recipe_black_king_bar",
+
+	----ag's scepter----
 	"item_point_booster",
 	"item_staff_of_wizardry",
 	"item_ogre_axe",
 	"item_blade_of_alacrity",
-
-	"item_mystic_staff",
-	"item_ultimate_orb",
-	"item_void_stone"
+	"item_recipe_ultimate_scepter",
+	
+	----heart of tarrasque----
+	"item_reaver",
+	"item_vitality_booster",
+	"item_ring_of_tarrasque",
+	"item_recipe_heart"
 }
 
 return Items
